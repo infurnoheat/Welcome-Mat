e095cde50c0c01e6181361534d1ebce3900555a7
infurnoheat <stevenleeyu@gmail.com>
2020-05-05 10:05:41 -0700

    Beginning Welcome Message

--- bot.js
+++ bot.js
@@ -10,10 +10,12 @@ bot.once('ready', () => {
 
 bot.login(auth.token);
 
-//On member entrance, send msg: Not working
+//On member entrance
+bot.on('guildMemberAdd' , member => {
+    bot.channel.find('name' , 'general').send('Welcome to the Family. What corp are you from? (Use !corp [corp name] [game username]');
+})
 
-
-//Role assignment: WIP
+//Role assignment and Name Change
 bot.on('message' , msg => {
     if(!msg.content.startsWith('!') || msg.author.bot){
         return;
@@ -22,26 +24,28 @@ bot.on('message' , msg => {
     var cmd = args[0].toLowerCase();
     switch(cmd){
         case 'corp':
-            var corpname = args[1].toLowerCase();
-            var name;
-            for(i = 2; i < args.length; i++){
-                name += args[i] + ' ';
+            var corpname = args[1];
+            var name ='';
+            name = args.slice(2).join(' ');
+            if(name == ''){
+                return msg.channel.send('Please use the format !corp [corp name] [game username]');
             }
             for(i = 0; i < corplist.length; i++){
-                if(corpname = corplist[i]){
-                    msg.channel.send('Welcome to ' + corpname);
-                    msg.member.roles.add(msg.guild.roles.cache.find(role => role.name.toLowerCase() == name.toLowerCase()))
-                    msg.member.setNickname(msg.member.user.username + '- ' + name);
-                } else if(i = conplist.length - 1){
+                if(corpname == corplist[i]){
+                    msg.channel.send('Welcome to ' + corpname + ', ' + name);
+                    msg.member.roles.add(msg.guild.roles.cache.find(role => role.name.toLowerCase() == corpname.toLowerCase()))
+                    msg.member.setNickname(name + '- ' + corpname);
+                    break;
+                } else if(i == corplist.length - 1){
                     msg.channel.send('Sorry, that corp is not part of the family.');
                 }
             }
             break;
         case 'help':
-            msg.channel.send('!corp [corpname] [gamename] --- Join a corp!');
+            msg.channel.send('!corp [corpname] [in game username] --- Join a corp!');
             break;
         default:
             msg.channel.send('Sorry, we do not have that command. View the list of commands with !help');
             break;
     }
-})
+});
