7ab0a0cf13ee78b40e500c89945301a0892b95a0
infurnoheat <stevenleeyu@gmail.com>
2020-05-06 01:26:19 -0700

    Messaging and List done

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var Discord = require('discord.js');
 var auth = require('./auth.json');
 var bot = new Discord.Client();
-var corplist = ['DPT' , 'Domicile'];
+var corplist = ['DPT' , 'Domicile' , 'Bloodtide' , 'Dynasty' , 'space-bar' , 'Beer' , 'Meadow' , 'District' , 'Dominion' , 'TradeSys' , 'Destiny' , 'TradePub' , 'Simply-Chaos' , 'helloWorld' , 'Diligent' , 'Kith', 'PPU' , 'Wayfarers'];
 
 // Initialize Discord Bot
 bot.once('ready', () => {
@@ -12,7 +12,8 @@ bot.login(auth.token);
 
 //On member entrance
 bot.on('guildMemberAdd' , member => {
-    bot.channel.find('name' , 'general').send('Welcome to the Family. What corp are you from? (Use !corp [corp name] [game username]');
+    var welcome = member.guild.channels.cache.get('706052898836054038');
+    welcome.send('Welcome to the Family. What corp are you from? (Use !corp [corpname] [in game username])');
 });
 
 //Role assignment and Name Change
@@ -32,7 +33,7 @@ bot.on('message' , msg => {
                 if(corpname == corplist[i]){
                     msg.channel.send('Welcome to ' + corpname + ', ' + name);
                     msg.member.roles.add(msg.guild.roles.cache.find(role => role.name.toLowerCase() == corpname.toLowerCase()))
-                    msg.member.setNickname(name + ' - ' + corpname);
+                    msg.member.setNickname(name + ' - ' + corplist[i]);
                     break;
                 } else if(i == corplist.length - 1){
                     msg.channel.send('Sorry, that corp is not part of the family.');
@@ -41,6 +42,7 @@ bot.on('message' , msg => {
             break;
         case 'help':
             msg.channel.send('!corp [corpname] [in game username] --- Join a corp!');
+            msg.channel.send('There may be some easter egg commands. Go find them');
             break;
         case 'execute':
             var order = '';
